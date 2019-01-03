c7eb15173e9adc8419f6ba63480a0e41af31fd4c
luben3485 <luben3485@gmail.com>
2019-01-03 16:35:24 +0800

    indoor location

--- UserInterface/test.py
+++ UserInterface/test.py
@@ -1,5 +1,7 @@
-import time
-from bluepy.btle import Scanner, DefaultDelegate
-import rssi
-
-rssi.RSSI()
+#import time
+#from bluepy.btle import Scanner, DefaultDelegate
+import rssi_
+rssi_.RSSI(1)
+#print(A)
+#A,B,C = rssi.RSSI()
+#print(A,B,C)
