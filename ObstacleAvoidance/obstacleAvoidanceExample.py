0dcfe5fdfe81594fab88faa2874a2c42ecbbaa87
luben <luben@gmail.com>
2019-05-26 08:19:33 +0100

    motor rpi3 modify obstacle avoidance code

--- ObstacleAvoidance/obstacleAvoidanceExample.py
+++ ObstacleAvoidance/obstacleAvoidanceExample.py
@@ -1,19 +1,19 @@
 import time
 import determineDistance
 import bubbleRebound
-#import kalman
+import kalman
 while True:
     determineDistance.get_distance()
 
-#	distanceKalman = kalman.kalmanFilter(determineDistance.distance)
+    distanceKalman = kalman.kalmanFilter(determineDistance.distance)
 	
-    for i in range(len(determineDistance.distance)):
-        print("before sensor %d %f" % (i,determineDistance.distance[i]))	
+#   for i in range(len(determineDistance.distance)):
+#       print("before sensor %d %f" % (i,determineDistance.distance[i]))	
 	
-#	for i in range(len(distanceKalman)):
-#		print("after sensor %d %f" % (i,distanceKalman[i]))	
-#	if bubbleRebound.checkObstacles(distanceKalman):
-#		turnAngle = bubbleRebound.calculateAngle(distanceKalman)
+    for i in range(len(distanceKalman)):
+        print("after sensor %d %f" % (i,distanceKalman[i]))	
+    if bubbleRebound.checkObstacles(distanceKalman):
+        turnAngle = bubbleRebound.calculateAngle(distanceKalman)
     if bubbleRebound.checkObstacles(determineDistance.distance):
         turnAngle = bubbleRebound.calculateAngle(determineDistance.distance)
         print("turnAngle:%f" % turnAngle)
