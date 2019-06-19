1a70aacfeaf10628d0700fe0d01d07588355a321
luben <luben@gmail.com>
2019-06-19 13:48:07 +0100

    user tracking

--- ObstacleAvoidance/utils.py
+++ ObstacleAvoidance/utils.py
@@ -1,11 +1,13 @@
-import determineDistance
-import bubbleRebound
-import kalman
+from . import determineDistance
+from . import bubbleRebound
+from . import kalman
 ini = 0
 pre = [0]*6
 def obstacleAvoidance(init = False):  
+    # pre=[0] * 6
     determineDistance.get_distance()
     distanceKalman = kalman.kalmanFilter(determineDistance.distance)
+    '''
     if(init):
         pre = determineDistance.distance
     else:
@@ -13,7 +15,7 @@ def obstacleAvoidance(init = False):
             if abs(pre[i]-determineDistance.distance[i]) >=70:
                 distanceKalman[i] = pre[i] 
             pre[i] = distanceKalman[i]
-            
+    '''     
     if bubbleRebound.checkObstacles(distanceKalman):
         turnAngle = bubbleRebound.calculateAngle(distanceKalman)
         return turnAngle
